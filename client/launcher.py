610e5945aca13fe8b2fa660f78536b2a64e3fdb2
Bo WANG <bo.wang@systrangroup.com>
2018-04-03 18:48:45 +0200

    take 0 as value if the queued_time is null (#3)
    * take 0 as value if the queued_time is null
    * take 0 as value if the queued_time is null for delete

--- client/launcher.py
+++ client/launcher.py
@@ -158,7 +158,7 @@ elif args.cmd == "lt":
     if not args.json:
         print("%-5s %-42s %-12s %-8s %-20s %-22s %-9s %s" %
               ("TYPE", "TASK_ID", "RESOURCE", "PRIORITY", "LAUNCH DATE", "IMAGE", "STATUS", "MESSAGE"))
-        for k in sorted(result, key=lambda k: float(k["queued_time"])):
+        for k in sorted(result, key=lambda k: float(k["queued_time"] or 0)):
             date = datetime.fromtimestamp(math.ceil(float(k["queued_time"]))).isoformat(' ')
             print("%-4s %-42s %-12s %6d   %-20s %-22s %-9s %s" %
                   (k["type"], k["task_id"], k["resource"], int(k["priority"]), 
@@ -331,7 +331,7 @@ elif args.cmd == "dt":
     if not args.json:
         print('Delete %d tasks:' % len(result))
         print("\t%-32s\t%-20s\t%-30s\t%-10s\t%s" % ("TASK_ID", "LAUNCH DATE", "IMAGE", "STATUS", "MESSAGE"))
-        for k in sorted(result, key=lambda k: float(k["queued_time"])):
+        for k in sorted(result, key=lambda k: float(k["queued_time"] or 0)):
             date = datetime.fromtimestamp(math.ceil(float(k["queued_time"]))).isoformat(' ')
             print("\t%-32s\t%-20s\t%-30s\t%-10s\t%s" % (
                 k["task_id"], date, k["image"], k["status"], k.get("message")))
