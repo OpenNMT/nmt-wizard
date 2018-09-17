5a7ef1f31b5ad08fe4b616b073856f65cbf9d8d8
Jean A. Senellart <jean.senellart@systrangroup.com>
2018-09-17 17:58:36 +0200

    change server version

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -8,7 +8,7 @@ import logging
 import time
 from redis.exceptions import ConnectionError
 
-VERSION = "0.3.0-ce"
+VERSION = "0.3.1-ce"
 def append_version(v):
     global VERSION
     VERSION += ":" + v
