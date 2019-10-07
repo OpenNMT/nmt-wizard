15da0da065ff676b2b2f02b0375da4b8cf8dca6a
nguyendc-systran <nguyendc@systran.fr>
2019-10-08 00:21:15 +0200

    update changlog and bump version v1.10.0

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,7 +9,7 @@ from nmtwizard.redis_database import RedisDatabase
 from nmtwizard import common
 from redis.exceptions import ConnectionError
 
-VERSION = "1.9.0"
+VERSION = "1.10.0"
 
 app = Flask(__name__)
 app._requestid = 1
