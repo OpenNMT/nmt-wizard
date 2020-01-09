844730784d570fc2c0876c0c0757b06773acfed3
dnhat.anh.bui <anhbn@rabiloo.com.vn>
2020-01-09 11:36:56 +0700

    Update style code for app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,7 +5,7 @@ import json
 
 from flask import Flask
 from flask_ini import FlaskIni
-from redis.exceptions import ConnectionError
+# from redis.exceptions import ConnectionError
 
 from nmtwizard.redis_database import RedisDatabase
 from nmtwizard import common
