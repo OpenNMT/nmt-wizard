e6e3ad7042d999b40e88d210c89880a9cf09a20c
Pierre-jean Liard <pierre-jean.liard@systrangroup.com>
2020-11-06 11:56:52 +0100

    refs #55271: Add current session parameter to render_template function

--- server/app/routes.py
+++ server/app/routes.py
@@ -139,7 +139,7 @@ def handle_error(e):
     if accept == 'json':
         return jsonify(error=str(e)), code
     else:
-        return render_template("error.html", code=code, title=e.name, message=e.description)
+        return render_template("error.html",infos=g.session, user=g.session['currentUser'], code=code, title=e.name, message=e.description)
 
 def accept_for_error():
     for mime_type,_ in request.accept_mimetypes:
