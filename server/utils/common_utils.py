849ae300c11ec7f5c5aafca808efba632cb1092d
Hùng Phương Ngô <phuongnh@rabiloo.com.vn>
2021-03-23 11:15:47 +0700

    refs #56253: handle train restricted folder permission

---
+++ server/utils/common_utils.py
@@ -0,0 +1,38 @@
+import re
+from flask import g
+
+from app import app
+
+
+def is_allow_access_train_restricted(path, permission):
+    if not is_resource_train_restricted(path):
+        return True
+
+    return check_permission_access_train_restricted(permission)
+
+
+def check_permission_access_train_restricted(permission):
+    # get entity code of user
+    entity_code = None
+    if g.get('user'):
+        entity_code = g.user.entity.entity_code
+
+    owner_code = app.get_other_config(['train_restricted', 'owner_code'])
+    partner_codes = app.get_other_config(['train_restricted', 'partner_codes'])
+
+    # user is OWNER, set full permission
+    if owner_code is None or entity_code == owner_code:
+        return True
+
+    # user is PARTNER partners, set allowed permission
+    if partner_codes is None or any(entity_code in partner['codes'] and permission in partner['permissions'] for partner in partner_codes):
+        return True
+
+    return False
+
+
+def is_resource_train_restricted(path):
+    regex_resource = '^shared_data:[a-z]{2}_[a-z]{2}\/train_restricted(\/.*)*$'
+    regex_config = '^\$\{SHARED_DATA_TRAIN_DIR\}/[a-z]{2}_[a-z]{2}\/train_restricted(\/.*)*$'
+
+    return re.search(regex_resource, path) or re.search(regex_config, path)
