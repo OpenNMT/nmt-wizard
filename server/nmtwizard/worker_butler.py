cbfa42a6c7e4283fa6395b40d0e1170832b5f44e
knguyen <kien.nguyen.external@systrangroup.com>
2022-03-30 15:15:33 +0700

    refs #58749: Call api task/terminate before terminating task with phase expired

--- server/nmtwizard/worker_butler.py
+++ server/nmtwizard/worker_butler.py
@@ -3,6 +3,7 @@ import os
 import signal
 import time
 import logging
+import requests
 
 from nmtwizard import task
 
@@ -25,6 +26,10 @@ class WorkerButler:
         def info(self, msg, *args, **kwargs):
             override_msg = self.override_msg(msg)
             return self._logger.info(override_msg, *args, **kwargs)
+
+        def warning(self, msg, *args, **kwargs):
+            override_msg = self.override_msg(msg)
+            return self._logger.warning(override_msg, *args, **kwargs)
 
     def __init__(self, redis, services, instance_id, work_cycle):
         service = next(iter(services))
@@ -56,6 +61,14 @@ class WorkerButler:
                         service = self._redis.hget('task:' + task_id, 'service')
                         if service in self._services:
                             self._logger.info('%s: task expired', task_id)
+                            auth_token = self._redis.hget('task:%s' % task_id, 'token')
+                            callback_url = self._services[service]._config.get('callback_url')
+                            if auth_token:
+                                callback_url = callback_url.replace("://", "://" + auth_token + ":x@")
+                            r = requests.get(os.path.join(callback_url, "task/terminate", task_id),
+                                             params={'phase': 'expired'})
+                            if r.status_code != 200:
+                                self._logger.warning('incorrect result from \'task/terminate\' service: %s' % r.text)
                             with self._redis.acquire_lock(task_id):
                                 task.terminate(self._redis, task_id, phase='expired')
                     # expired in the queue - comes back in the work queue
